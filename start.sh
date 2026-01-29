3381d4aaf1a0baee1946eec623dccafb6f580154
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-01-29 05:32:09 +0300

    Allow branch selection in start.sh script
    Added support for specifying a branch name as an argument to start.sh, defaulting to 'main' if not provided. This enables more flexible cloning of the status repository.

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 
+BRANCH_NAME="${1:-main}"
 
-rm -rf repos/status && git clone https://github.com/MrIbrahem/status.git -b update repos/status
+rm -rf repos/status && git clone https://github.com/MrIbrahem/status.git -b {$BRANCH_NAME} repos/status
 
 python3 repos/status/start.py
